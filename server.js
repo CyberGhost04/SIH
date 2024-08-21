1cee6213db913a8e0aaae2845f2e1bce2953b309
CyberGhost04 <sidhantbahuguna@gmail.com>
2024-08-21 17:03:45 +0530

    completed

--- server.js
+++ server.js
@@ -4,12 +4,28 @@ const DB = require("./DB");
 const app = express();
 const port = 8060;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     
     DB.update();
     const users = DB.users;
     res.status(200).json(users);
 
+});
+
+app.put('/', async(req, res) => {
+    const data = req.body;
+    try{
+
+        DB.update();
+        DB.users.push(data);
+        DB.save();
+        res.status(200).json({scs : "true"});
+
+    }catch(e){
+        console.log(e);
+    }
 });
 
 app.listen(port, () => {
